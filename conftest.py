49675fbd024faf7633e5d3d84a48cde277492e9a
lihn2phan <Fynbakee1@>
2023-06-06 12:37:34 +0300

    Реализован тест для создания группы без предусловий

---
+++ conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from fixture.application import Application
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application("C:\\Tools\\AddressBook\\AddressBook.exe")
+    request.addfinalizer(fixture.destroy)
+    return fixture
